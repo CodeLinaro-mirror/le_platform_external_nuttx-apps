60bfc6037027d0262ada2ad9b40ed7a36b67c78e
Pengshun Zhao <quic_pengshun@quicinc.com>
2023-08-01 18:36:21 +0800

    nuttx-app: add ros_com driver
    add ros_com driver
    
    Change-Id: I538f86397290b425ee37d43b4e9ee81464f224fd
    Signed-off-by: Pengshun Zhao <quic_pengshun@quicinc.com>

---
+++ qtiamr/roscom/ros_com.h
@@ -0,0 +1,15 @@
+/***************************************************************************
+* Copyright (c) 2023 Qualcomm Innovation Center, Inc. All rights reserved.
+* SPDX-License-Identifier: BSD-3-Clause-Clear
+****************************************************************************/
+#include <stdio.h>
+#include <stdint.h>
+
+#ifdef CONFIG_APP_QCOMAMR
+
+/* roscom task running frequency */
+
+int ros_com_task(int argc, char *argv[]);
+
+#endif
+#endif
