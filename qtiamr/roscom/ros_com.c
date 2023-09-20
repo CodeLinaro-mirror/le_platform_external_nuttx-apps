4e54a019c4a6a0c3699f697cb9610356dc0e90d3
Bella Zhao <quic_qiazhao@quicinc.com>
2023-09-20 20:11:07 +0800

    nuttx-app: imu: add imu data publish

--- qtiamr/roscom/ros_com.c
+++ qtiamr/roscom/ros_com.c
@@ -15,6 +15,7 @@
 #include "main.h"
 #include "motion_task.h"
 #include "motor_driver.h"
+#include "imu.h"
 
 extern volatile struct amr_motion_s g_amr_motion;
 
@@ -550,7 +551,41 @@ int publish_pos_odom_to_ros(void)
 
 
 	return OK;
+}
 
+int publish_imu_to_ros(void)
+{
+	int ret =0;
+
+	struct extend_frame_s send_data =  {0};
+	static struct timespec tp;
+
+	clock_gettime(CLOCK_MONOTONIC, &tp);
+
+	send_data.frame_header = EXTEND_FRAME_HEADER;
+	send_data.cmd = CMD_IMU;
+	send_data.frame_tail = EXTEND_FRAME_TRAILER;
+
+	send_data.data1 = (tp.tv_sec >> 24) & 0xff;
+	send_data.data2 = (tp.tv_sec >> 16) & 0xff;
+	send_data.data3 = (tp.tv_sec >> 8) & 0xff;
+	send_data.data4 = (tp.tv_sec) & 0xff;
+	send_data.data5 = ((tp.tv_nsec/1000000) >> 8) & 0xff;
+	send_data.data6 = (tp.tv_nsec/1000000) & 0xff;
+
+	memcpy(&send_data.data7, get_imu_data(), 12);
+
+	send_data.check_sum = data_check_sum(&send_data, sizeof(struct extend_frame_s) - 2);
+	ret = ros_cmd_send(&send_data, sizeof(struct extend_frame_s));
+
+	if (ret < 0)
+	{
+		syslog(LOG_WARNING, "publish imu sensor data failed!\n");
+		return ERROR;
+	}
+
+	syslog(LOG_DEBUG, "publish imu data: accel_x(%x, %x), accel_y(%x,%x)\n", send_data.data7, send_data.data8, send_data.data9, send_data.data10);
+	return OK;
 }
 
 int publish_status_to_ros(void)
@@ -763,6 +798,10 @@ int ros_com_task(int argc, char *argv[])
 				if (j++ % ROSCOM_FREQUENCY == 0)
 				{
 					publish_status_to_ros();
+				}
+				if(!amr_imu_check())
+				{
+					publish_imu_to_ros();
 				}
 			}
 			if (g_amr_ros.notify_mode_switch)
