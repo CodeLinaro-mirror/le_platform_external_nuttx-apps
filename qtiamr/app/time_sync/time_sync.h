8f0fe3bd7fef5395ff249e011cf3b27a98d3554c
jionzhao <quic_jionzhao@quicinc.com>
2023-12-29 15:49:37 +0800

    nuttx-aps: add qtiamr robot control applications
    add charger controller & robot configuration &
    emergency avoidance & client & time sync applications
    for AMR robot control.
    
    Change-Id: Ic1ca44851d8f2dd5244611a424a19eeebd249479
    Signed-off-by: jionzhao <quic_jionzhao@quicinc.com>

---
+++ qtiamr/app/time_sync/time_sync.h
@@ -0,0 +1,13 @@
+/****************************************************************************
+ *
+ * Copyright (c) 2024 Qualcomm Innovation Center, Inc. All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause-Clear
+ *
+ ****************************************************************************/
+
+#ifndef __TIME_SYNC_H
+#define __TIME_SYNC_H
+
+/* IMU application header */
+
+#endif
