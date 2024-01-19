dbe8f824e7fddfaa10ed0a0a0f0ac0d6bb953734
keqili <quic_keqili@quicinc.com>
2024-01-19 16:59:40 +0800

    nuttx-apps: qtiamr: add charger device file
    Signed-off-by: keqili <quic_keqili@quicinc.com>

--- qtiamr/module/charger/charger_device/charger_device.h
+++ qtiamr/module/charger/charger_device/charger_device.h
@@ -8,65 +8,76 @@
 #ifndef __APP_CHARGER_DEVICE_H
 #define __APP_CHARGER_DEVICE_H
 
+
 /****************************************************************************
  * Included Files
  ****************************************************************************/
+ 
 #include <signal.h>
+#include <stdint.h>
 #include <nuttx/signal.h>
+#include "charger_sm.h"
 #include "amr_signal.h"
+#include "charger_management.h"
+#include "main.h"
+
 
 /********************************************************************************
  * Pre-processor Definitions
  ********************************************************************************/
+#define CHARGER_DEVICE_NAME_SIZE        20
 
-#define CHARGER_DEVICE_NAME_SIZE  20
-#define BATT_VOLT_NOTIFY_2S       2000000
+#define CHARGER_SM_PRIORITY         DEFAULT_PRIORITY
+#define CHARGER_SM_STACKSIZE        DEFAULT_STACK_SIZE
 
-
-typedef struct charger_driver_ops_s {
-	CODE int (*get_voltage)(float *volt);
-	CODE bool (*get_current)(float *curr);
-	CODE bool (*get_wheel_speed)(float *vx, float *vz);
-	CODE int (*set_def_speed)(float vx, float vz);
-	CODE int (*chr_pile_sig_stat)(bool *stat);
-	CODE int (*chr_if_charging)(bool *stat);
-}chr_drv_ops_t;
-
-typedef struct charger_notify_ops_s {
-	CODE int (*qrc_notify_callback)(void * data, size_t len, bool ack);
-	CODE int (*motion_speed_nofity)(float vx, float vz);
-}chr_notify_ops_t;
-
-struct voltage_cmd_s
-{
-  uint8_t cmd_type;
-  float   cmd_value;
-};
+/*polling time definition*/
+#define CHARGER_EXCEPTION_DELAY     10000000  //10s
+#define IDLE_BATT_VOLT_NOTIFY       5000000   //5s
+#define CHARGING_DONE_DELAY         500000   //500ms  ==> need to debug
+#define CONTROLLING_SPEED_NOTIFY    20000     //20ms
+#define CHARGING_POLL_DELAY         2000000   //2s
+#define CHR_CTL_MODE_TIMEOUT        SEC2TICK(200)
 
 
-typedef struct charger_device_s {
-	struct amr_signal_s charger_signal;
-	bool initialized;
-	char name[CHARGER_DEVICE_NAME_SIZE];
-	chr_sm_state_t sm;
-	const chr_notify_ops_t *nfy_ops;
-	const chr_drv_ops_t *drv_ops;
-  pthread_mutex_t cc_mutex;
+/****************************************************************************
+ * Public Types
+ ****************************************************************************/
 
+/*charger dev main struct*/
+struct charger_device_s{
+  struct amr_signal_s charger_signal;
+  bool initialized;
+  char name[CHARGER_DEVICE_NAME_SIZE];
+  struct charger_sm_state_s sm;
+  float full_battery_voltage;
+  float low_battery_voltage;
+  float battery_capacity;
+  float battery_voltage;
+  pthread_rwlock_t  polling_rw_lock;
+  uint32_t polling_interval;
+  notification_cb notify_callback;
+  motion_speed_cb motion_callback;
   
-}__attribute__((align(4)))charger_dev_t;
+}__attribute__((aligned(4)));
 
+/****************************************************************************
+ * Public Function Prototypes
+ ****************************************************************************/
 
-void chr_core_drv_ops_register(const char *name,const chr_drv_ops_t *ops);
-void chr_core_notify_ops_register(const chr_drv_ops_t *ops);
-float chr_core_get_voltage();
-float chr_core_get_charging_curr();
-bool chr_core_get_pile_stats();
-
-int chr_core_sm_signal_init();
-int chr_core_sm_signal_send();
-int chr_core_sm_signal_wait(struct siginfo *info, FAR const struct timespec *timeout);
-
+int charger_dev_sm_signal_send(int32_t sigval_int);
+int charger_dev_sm_signal_wait(struct siginfo *info, FAR const struct timespec *timeout);
+void charger_dev_sm_signal_init(void);
+void set_polling_interval(uint32_t interval);
+void charger_dev_rwlock_init(pthread_rwlock_t *cc_rw_lock);
+void charger_dev_rwlock_rdlock_acquire(FAR pthread_rwlock_t *cc_rw_lock);
+void charger_dev_rwlock_wrlock_acquire(FAR pthread_rwlock_t *cc_rw_lock);
+void charger_dev_rwlock_release(FAR pthread_rwlock_t *rw_lock);
+void charger_exception_notify(enum charger_exception_type_e exception_err);
+void sm_state_notify(enum chr_sm_st_e state);
+void battery_voltage_notify(float voltage);
+void voltage_change_notify(void);
+void charging_current_notify(float current);
+void motion_speed_notify(float vx, float vz);
 
 
 #endif /* __APP_CHARGER_DEVICE_H */
