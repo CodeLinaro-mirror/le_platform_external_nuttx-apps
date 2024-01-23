b3fa7483b2f9b7f82fa6ee2c6d27f2694dd92153
keqili <quic_keqili@quicinc.com>
2024-01-23 11:18:35 +0800

    nuttx-apps: qtiamr: add charger qrc message
    Signed-off-by: keqili <quic_keqili@quicinc.com>

--- qtiamr/libqrc/include/app_msg/charger_control_msg.h
+++ qtiamr/libqrc/include/app_msg/charger_control_msg.h
@@ -5,9 +5,9 @@
  *
  ****************************************************************************/
 
-
 #ifndef __APP_CHARGER_CONTROL_MSG_H
 #define __APP_CHARGER_CONTROL_MSG_H
+
 /****************************************************************************
  * Included Files
  ****************************************************************************/
@@ -18,37 +18,57 @@
  * Pre-processor Definitions
  ********************************************************************************/
 
-#define CHARGER_MSG_PIPE "motion"
+#define CHARGER_PIPE "charger"
 
 enum charger_ctl_cmd_e
 {
-	GET_VOLTAGE 	= 	0x01,
-	GET_FULL_BATT_VOLT	=	0x02,
-	GET_CURRENT 	= 	0x03,
-	GET_PILE_STATS 	= 	0x04,
-	GET_SM_STATS	=	0x05,
-	START_CHARGING	=	0x06,
-	STOP_CHARGING	=	0x07,
-	GET_BATT_CAP	=	0x08
+	GET_CTL_VOLTAGE = 10,
+	GET_CTL_CURRENT,
+	GET_CTL_SM_STATE,
+	GET_CTL_EXCEPTION,
+	GET_CTL_PILE_STATE,
+	GET_CTL_IS_CHARGING,
+	GET_CTL_ALL_STATE,
+	START_CTL_CHARGING,
+	STOP_CTL_CHARGING
 };
 
-
-union cmd_val
+enum charger_ctl_exception_e
 {
-  float		voltage;
-  float		current;
-  uint32_t	pile_stats;
-  uint32_t	sm_stats;
+  CHARGER_CTL_VOLTAGE_ERROR = 1,
+  CHARGER_CTL_CHARGING_CURRENT_ERROR,
+  CHARGER_CTL_PILE_STAT_ERROR,
+  CHARGER_CTL_IS_CHARGING_ERROR,
+  CHARGER_CTL_GET_SPEED_ERROR,
+  CHARGER_CTL_SM_STATE_ERROR,
+  CHARGER_CTL_DEV_NOT_READY_ERROR,
+  CHARGER_CTL_ENTER_EXCEPTION,
+  CHARGER_CTL_GET_ALL_ERROR
 };
 
-
-
-struct charger_ctl_cmd_s
+enum charger_ctl_sm_state_e
 {
-  uint32_t cmd_type;
-  union cmd_val cmd_value;
-}__attribute__((align(4)));
+  CHR_CTL_SM_IDLE = 1,
+  CHR_CTL_SM_SEARCHING,
+  CHR_CTL_SM_CONTROLLING,
+  CHR_CTL_SM_FORCE_CHARGING,
+  CHR_CTL_SM_CHARGING,
+  CHR_CTL_SM_CHARGER_DONE,
+  CHR_CTL_SM_EXCEPTION
+};
 
+struct charger_ctl_msg_s
+{
+  enum charger_ctl_cmd_e cmd_type;
+  union
+    {
+      float voltage;
+      float current;
+      uint32_t pile_stats;
+      uint32_t is_charging;
+      uint32_t sm_state;
+      enum charger_ctl_exception_e exception_value;
+    }cmd_data;
+}__attribute__((aligned(4)));
 
 #endif /* __APP_CHARGER_CONTROL_MSG_H */
-
